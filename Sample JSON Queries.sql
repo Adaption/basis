ff99dfbce7a23df60147972956f5abc86ece90f0
anh-khue <thailyanhkhue@gmail.com>
2018-05-04 21:30:56 +0700

    Change sample data structure for JSON field and queries for clearer look from issue [#2]. Preparing for issue [#13].

--- Sample JSON Queries.sql
+++ Sample JSON Queries.sql
@@ -1,6 +1,19 @@
 #Tìm các sản phẩm của Razer ~ Tìm theo key/value trong JSON
-SELECT * FROM product WHERE JSON_CONTAINS(attributes, '{"data": "Razer"}', "$.attributes");
+SELECT *
+FROM product
+WHERE JSON_CONTAINS(attributes, '{"data": "Razer"}', "$.attributes");
+
+# Query after modifying sample data
+SELECT *
+FROM product
+WHERE attributes->'$."Nhà sản xuất"' LIKE '%Razer%';
 
 #Tìm các mẫu CHUỘT của Razer
-SELECT * FROM product WHERE JSON_CONTAINS(attributes, '{"data": "Razer"}', "$.attributes") AND category_id = 1;
+SELECT *
+FROM product
+WHERE JSON_CONTAINS(attributes, '{"data": "Razer"}', "$.attributes") AND category_id = 1;
+
+SELECT *
+FROM product
+WHERE attributes->'$."Nhà sản xuất"' LIKE '%Razer%' AND category_id = 1;
 
