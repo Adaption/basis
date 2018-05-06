a30392d9b4a96c75726ebf9991569506191633d9
anh-khue <thailyanhkhue@gmail.com>
2018-05-06 22:18:46 +0700

    Remove old json-handle project in [#13].

--- microservices/json-query-handler/src/main/java/io/basis/jsonqueryhandler/repository/CategoryRepository.java
+++
@@ -1,8 +0,0 @@
-package io.basis.jsonqueryhandler.repository;
-
-import io.basis.jsonqueryhandler.model.Category;
-import org.springframework.data.repository.CrudRepository;
-
-public interface CategoryRepository extends CrudRepository<Category, Integer> {
-
-}
