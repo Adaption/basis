82ea68dcbde5f582f09bd920e5db2596f3aa77e1
nhat7490 <nhatnqse62245@fpt.edu.vn>
2018-05-11 10:15:51 +0700

    Return Response Entity in controller

--- post-service/src/main/java/io/basis/postservice/controller/PostController.java
+++ post-service/src/main/java/io/basis/postservice/controller/PostController.java
@@ -2,10 +2,15 @@ package io.basis.postservice.controller;
 
 import io.basis.postservice.model.Post;
 import io.basis.postservice.service.PostService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
+import static org.springframework.http.HttpStatus.*;
+import static org.springframework.http.ResponseEntity.*;
 
 @RestController
 public class PostController {
@@ -17,33 +22,44 @@ public class PostController {
     }
 
     @GetMapping("/post/{id}")
-    public Post getById(@PathVariable("id") int id) {
-        return this.postService.getById(id);
+    public ResponseEntity<Post> getById(@PathVariable("id") int id) {
+        return this.postService.getById(id) != null ?
+                status(OK).body(postService.getById(id)) :
+                status(NOT_FOUND).build();
     }
 
     @GetMapping("/post")
-    public List<Post> getAll() {
-        return this.postService.getAll();
+    public ResponseEntity<List<Post>> getAll() {
+        return this.postService.getAll().isEmpty() ?
+                status(NO_CONTENT).build():
+                status(OK).body(postService.getAll());
+
     }
 
     @PostMapping(value = "/post", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public void create(@RequestBody Post post) {
+    public ResponseEntity create(@RequestBody Post post) {
         if (this.postService.getById(post.getPostId()) == null){
             this.postService.create(post);
+            return status(CREATED).build();
         }
+        return status(BAD_REQUEST).build();
     }
 
     @DeleteMapping(value = "/post/{id}")
-    public void delete(@PathVariable("id") int id) {
+    public ResponseEntity delete(@PathVariable("id") int id) {
         if (this.postService.getById(id) != null) {
             this.postService.delete(id);
+            return status(OK).build();
         }
+        return status(BAD_REQUEST).build();
     }
 
-    @PutMapping(value = "/media/{id}/", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public void update(@RequestBody Post post){
+    @PutMapping(value = "/media/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity update(@RequestBody Post post){
         if (this.postService.getById(post.getPostId()) != null) {
             this.postService.update(post);
+            return status(OK).build();
         }
+        return status(BAD_REQUEST).build();
     }
 }
