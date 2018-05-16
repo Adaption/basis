1edd0fe200e1e7f09652abd0119a86aa77b859cc
MinhQuan <buikhacminhquan@hotmail.com>
2018-05-16 22:42:19 +0700

    update less/style.less

--- basis-ui/web-admin-home/src/App.js
+++ basis-ui/web-admin-home/src/App.js
@@ -7,27 +7,43 @@ import './style.css';
 class Topbar extends Component{
   render(){
     return(
-      <table className='top-nav'>
-        <tbody>
-          <tr>
-            <td>
-              <img className='logo' src={logo} />
-            </td>
-            <td>POS</td>
-            <td>WEB</td>
-            <td>OMNI CHANNEL</td>
-            <td>BẢNG GIÁ</td>
-            <td>THÊM</td>
-            <td>Trợ giúp</td>
-            <td>Đăng nhập</td>
-            <td>
-              <button>
-                Dùng thử
-              </button>
-            </td>
-          </tr>
-        </tbody>
-      </table>
+      // <table className='top-nav'>
+      //   <tbody>
+      //     <tr>
+      //       <td>
+      //         <img className='logo' src={logo} />
+      //       </td>
+      //       <td>POS</td>
+      //       <td>WEB</td>
+      //       <td>OMNI CHANNEL</td>
+      //       <td>BẢNG GIÁ</td>
+      //       <td>THÊM</td>
+      //       <td>Trợ giúp</td>
+      //       <td>Đăng nhập</td>
+      //       <td>
+      //         <button>
+      //           Dùng thử
+      //         </button>
+      //       </td>
+      //     </tr>
+      //   </tbody>
+      // </table>
+      
+      <div className='top-nav'>
+        <div>
+          <img className='logo' src={logo}/>
+        </div>
+        <div className='left'>POS</div>
+        <div className='left'>WEB</div>
+        <div className='left'>OMNI CHANNEL</div>
+        <div className='left'>BẢNG GIÁ</div>
+        <div className='left'>THÊM</div>
+        <div className='right'>Trợ giúp</div>
+        <div className='right'>Đăng nhập</div>
+        <div className='right'>
+            Dùng thử
+        </div>
+      </div>
     );
   }
 }
